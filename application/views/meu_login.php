2e692bfc8c04469fa7b326840df57d00e564e6d9
Julio Cesar <juliocs78383@gmail.com>
2022-04-01 18:15:58 -0300

    Preparando a tela de login

--- application/views/meu_login.php
+++ application/views/meu_login.php
@@ -6,60 +6,7 @@
   <title>Login</title>
 <body>
 
-<link href="/application/login.css" rel="stylesheet">
-<script type="text/javascript">
-  var current = null;
-document.querySelector('#email').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: 0,
-      duration: 700,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '240 1386',
-      duration: 700,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-document.querySelector('#password').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: -336,
-      duration: 700,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '240 1386',
-      duration: 700,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-document.querySelector('#submit').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: -730,
-      duration: 700,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '530 1386',
-      duration: 700,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-
-</script>
-
+<link href="views/login.css" rel="stylesheet">
 
 <div class="page">
   <div class="container">
@@ -104,9 +51,6 @@ document.querySelector('#submit').addEventListener('focus', function(e) {
 </div>
 </body>
 </html>
-
-
-
 
 <style>
 
